46dd7e57af164a0a3ce7b1b86a801ae8321f9f1b
Jiří Hausner <87451918+jirihausner@users.noreply.github.com>
2025-01-02 10:13:38 +0100

    upgrade zio 2.1.14, jsoniter-scala-circe 2.32.0, sbt-ci-release 1.9.2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -35,9 +35,9 @@ object BuildHelper {
 
     val circe           = "0.14.10"
     val circeDerivation = "0.13.0-M5"
-    val jsoniter        = "2.31.3"
+    val jsoniter        = "2.32.0"
     val scalaJavaTime   = "2.6.0"
-    val zio             = "2.1.13"
+    val zio             = "2.1.14"
     val zioSchema       = "1.5.0"
   }
 
