e8c6e27da795163ae6105427d8798e8c3b71c03e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-01-12 11:36:30 +0100

    Update jsoniter-scala-circe to 2.33.0 (#3)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -35,7 +35,7 @@ object BuildHelper {
 
     val circe           = "0.14.10"
     val circeDerivation = "0.13.0-M5"
-    val jsoniter        = "2.32.0"
+    val jsoniter        = "2.33.0"
     val scalaJavaTime   = "2.6.0"
     val zio             = "2.1.14"
     val zioSchema       = "1.5.0"
