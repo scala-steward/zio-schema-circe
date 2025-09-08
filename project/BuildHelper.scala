6afbdb76eac52acd1d5b313953197829b721b12c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-09-08 15:04:25 +0200

    Update zio, zio-streams, zio-test, ... to 2.1.21 (#117)
    Co-authored-by: Jiří Hausner <87451918+jirihausner@users.noreply.github.com>

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -37,7 +37,7 @@ object BuildHelper {
     val circeDerivation = "0.13.0-M5"
     val jsoniter        = "2.37.8"
     val scalaJavaTime   = "2.6.0"
-    val zio             = "2.1.20"
+    val zio             = "2.1.21"
     val zioSchema       = "1.7.4"
   }
 
