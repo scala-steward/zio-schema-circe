d3a080c17e3a4b413a2863d96ad1f474dd5fd419
Scala Steward <scala_steward@virtuslab.com>
2026-02-26 18:34:42 +0000

    Update zio-schema, zio-schema-derivation, ... to 1.8.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -38,7 +38,7 @@ object BuildHelper {
     val jsoniter        = "2.38.9"
     val scalaJavaTime   = "2.6.0"
     val zio             = "2.1.24"
-    val zioSchema       = "1.7.6"
+    val zioSchema       = "1.8.1"
   }
 
   def compilerOptions(scalaVersion: String, optimize: Boolean) = {
