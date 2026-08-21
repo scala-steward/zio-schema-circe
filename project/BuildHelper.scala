f7cc8cbe8a4661921caa03244a47048a0bdf2a3f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-08-21 14:05:21 +0200

    Update scala-java-time, ... to 2.7.0 (#210)
    Co-authored-by: Jiří Hausner <87451918+jirihausner@users.noreply.github.com>

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -36,7 +36,7 @@ object BuildHelper {
     val circe           = "0.14.15"
     val circeDerivation = "0.13.0-M5"
     val jsoniter        = "2.38.9"
-    val scalaJavaTime   = "2.6.0"
+    val scalaJavaTime   = "2.7.0"
     val zio             = "2.1.26"
     val zioSchema       = "1.8.5"
   }
