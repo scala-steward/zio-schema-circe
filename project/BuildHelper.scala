1c2b763a55d51697f2f33357da45a9e8cd51967b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-11 14:04:00 +0200

    Update zio-schema, zio-schema-derivation, ... to 1.8.7 (#240)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -38,7 +38,7 @@ object BuildHelper {
     val jsoniter        = "2.38.17"
     val scalaJavaTime   = "2.7.0"
     val zio             = "2.1.26"
-    val zioSchema       = "1.8.6"
+    val zioSchema       = "1.8.7"
   }
 
   def compilerOptions(scalaVersion: String, optimize: Boolean) = {
